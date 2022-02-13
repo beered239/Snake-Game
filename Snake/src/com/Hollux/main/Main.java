2b71fefbdd3481215810b1643d3705f5fdcd35f6
xHollowCellX <48739278+beered239@users.noreply.github.com>
2022-02-12 20:56:51 -0500

    Enables hardware acceleration for all systems
    - works for linux
    - windows enables openGL hardware acceleration be default
    - mac untested

--- Snake/src/com/Hollux/main/Main.java
+++ Snake/src/com/Hollux/main/Main.java
@@ -3,7 +3,7 @@ package com.Hollux.main;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.setProperty("sun.java2d.opengl", "true");
 		GameFrame frame = new GameFrame();
 		frame.getDefaultCloseOperation();
 	}
